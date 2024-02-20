047371e6b090224d60526b4e0f08dc011dd7787f
Aditya Gundla <151802795+Aditya178510@users.noreply.github.com>
2024-02-20 22:51:47 +0530

    Add files via upload

---
+++ one.js
@@ -0,0 +1,14 @@
+gsap.from(".ch",{
+    x:-100,
+    duration:2,
+    delay: 0.5,
+    stagger: 0.3,
+    opacity:0,
+})
+gsap.from(".image",{
+    x:-100,
+    duration:2,
+    delay: 0.5,
+    stagger: 0.3,
+    opacity:0,
+})
